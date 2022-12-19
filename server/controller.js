03dd2710b23ad11c4a53061e91fafe6457dca8ed
sajahmadi <sajedehahmadi@gmail.com>
2022-12-18 22:14:13 -0600

    edited

--- server/controller.js
+++ server/controller.js
@@ -16,43 +16,37 @@ const sequelize = new Sequelize ( CONNECTION_STRING, {
 
 module.exports = {
   getCountries: (req, res) => {
-        sequelize.query( SELECT * FROM countries)
+        sequelize.query( `SELECT * FROM countries`)
         .then((dbRes) => {
             res.status(200).send(dbRes[0])
          })
          .catch(err => console.log(err))
     },
 
-createCity: (req, res) => {
-   const { name, rating, countryId } = req.body;
-   sequelize.query(
-    `INSERT INTO cities (name, rating, countryId) VALUES (:name, :rating, :countryId)`,
-    {
-      replacements: { name, rating, countryId },
-    }
-  )
-  .then(() => {
-     res.sendStatus(200);
-    })
-     .catch((error) => {
-       res.sendStatus(500);
-      console.error(error);
-    });
-},
+ createCity: (req, res) => {
+        let {name, rating, countryId} = req.body
+       
+        sequelize.query( `INSERT INTO cities (name, rating, country_id`)
+        values ('${name}','${rating}','${countryId})`))
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        
+        .catch(err => console.log(err))
+    },
+  };
 
 
   deleteCity: (req, res) => {
     const { id } = req.params
-        sequelize.query( DELETE FROM cities WHERE city_id = '${id}')
+        sequelize.query( `DELETE FROM cities WHERE city_id = '${id}'`)
         //what is this error for
         .then((dbRes) => {
             res.status(200).send(dbRes[0])
         })
         .catch(err => console.log(err))
     },
-getCities: (req, res) => {
-    sequelize.query( `SELECT cities.city_id, cities.name AS city, cities.rating,
-    countries.country_id, countries.name AS country
+getCities:(req, res) => {
+    sequelize.query( `SELECT cities.city_id, cities.name as city, cities.rating,
+    countries.country_id, countries.name as country
     FROM cities
     JOIN countries
     ON cities.country_id = countries.country_id`)
